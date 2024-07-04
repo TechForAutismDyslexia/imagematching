2484514cf022f8a6e627ba62394b58b1dc2af126
ELECTROMONKEY24 <yssmc24@gmail.com>
2024-07-04 20:15:04 +0530

    issuesfix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Imagedisplay from "./Components/Imagedisplayer";
 import "./Components/Imagedisplay.css";
+import Result from "./Components/Result";
 const App = () => {
 
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Router basename="/games/matching">
         <Routes>
           <Route path="/" element={<Imagedisplay />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
       </Router>
     </div>
